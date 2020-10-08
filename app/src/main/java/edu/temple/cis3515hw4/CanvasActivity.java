4f54bda0b016fd146a1010a693f939a6e318535e
Rory Giszter <tuc73313@temple.edu>
2020-10-08 08:51:57 -0400

    made CanvasActivity

--- app/src/main/java/edu/temple/cis3515hw4/CanvasActivity.java
+++ app/src/main/java/edu/temple/cis3515hw4/CanvasActivity.java
@@ -1,4 +1,24 @@
 package edu.temple.cis3515hw4;
 
-public class CanvasActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class CanvasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canvas);
+        Intent colorIntent = getIntent();
+        String color = colorIntent.getStringExtra("color");
+        View layout = findViewById(R.id.layout);
+        layout.setBackgroundColor(Color.parseColor(color));
+        TextView text = findViewById((R.id.colorText));
+        text.setText(color);
+    }
 }
