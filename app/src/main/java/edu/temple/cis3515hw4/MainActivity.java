2f21bea8d95ae9cf87f4606da99c59042299845d
Rory Giszter <tuc73313@temple.edu>
2020-10-22 08:06:17 -0400

    Changed files from Activities to Fragments

--- app/src/main/java/edu/temple/cis3515hw4/MainActivity.java
+++ app/src/main/java/edu/temple/cis3515hw4/MainActivity.java
@@ -3,20 +3,18 @@ package edu.temple.cis3515hw4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.TextView;
 
-public class PaletteActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_palette);
+        setContentView(R.layout.activity_main);
         final GridView grid = findViewById(R.id.gridview);
         grid.setNumColumns(3);
         String[] colors = getResources().getStringArray(R.array.colors);
@@ -29,7 +27,7 @@ public class PaletteActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                     TextView text = (TextView) view;
-                    Intent colorIntent = new Intent(PaletteActivity.this, CanvasActivity.class);
+                    Intent colorIntent = new Intent(MainActivity.this, CanvasActivity.class);
                     colorIntent.putExtra("color", text.getText());
                     colorIntent.putExtra("color_value", getResources().getStringArray(R.array.colors_values)[position]);
                     startActivity(colorIntent);
