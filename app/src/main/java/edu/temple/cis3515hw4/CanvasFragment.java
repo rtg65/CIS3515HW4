cea780184449030b52415101b08ecb9e4e2f9196
Rory Giszter <tuc73313@temple.edu>
2020-10-22 08:14:41 -0400

    Made CanvasFragment

--- app/src/main/java/edu/temple/cis3515hw4/CanvasFragment.java
+++ app/src/main/java/edu/temple/cis3515hw4/CanvasFragment.java
@@ -1,5 +1,6 @@
 package edu.temple.cis3515hw4;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,12 +19,12 @@ public class CanvasFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM1 = "color";
+    private static final String ARG_PARAM2 = "color_value";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String fColor;
+    private String fColorValue;
 
     public CanvasFragment() {
         // Required empty public constructor
@@ -32,16 +34,16 @@ public class CanvasFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param color Parameter 1.
+     * @param colorValue Parameter 2.
      * @return A new instance of fragment CanvasFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CanvasFragment newInstance(String param1, String param2) {
+    public static CanvasFragment newInstance(String color, String colorValue) {
         CanvasFragment fragment = new CanvasFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM1, color);
+        args.putString(ARG_PARAM2, colorValue);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +52,8 @@ public class CanvasFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            fColor = getArguments().getString(ARG_PARAM1);
+            fColorValue = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -59,6 +61,13 @@ public class CanvasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        // Retrieve stored color value
+
+        // Change text value
+        ((TextView) container.findViewById(R.id.colorText)).setText(fColor);
+
+        // Change layout background color
+        container.findViewById(R.id.layout).setBackgroundColor(Color.parseColor(fColorValue));
         return inflater.inflate(R.layout.fragment_canvas, container, false);
     }
 }
